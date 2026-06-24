fd00a31f0976c7159384163bb10bd8fccaafff69
Indrasena Reddy <indrasenareddygummireddy2005@gmail.com>
2026-06-24 22:01:09 +0530

    practised functions

--- practise2/function/funexp.py
+++ practise2/function/funexp.py
@@ -6,12 +6,25 @@
 
 # def greet(name):
 #     print("Hello ",name)
-
 # greet("Rahul Gandi")
 
 
-def add_num(num1,num2):
-    sum=num1+num2
-    print("Sum: ",sum)
+# def add_num(num1,num2):
+#     sum=num1+num2
+#     print("Sum: ",sum)
+# add_num(10,20)
 
-add_num(10,20)
+
+# def find_square(num):
+#     square = num*num
+#     return square
+# square = find_square(5)
+# print("Square: ",square)
+
+#build in functions
+import math
+square_root = math.sqrt(144)
+print("Square root: ",square_root)
+power = pow(7,3)
+print("power: ",power)
+
